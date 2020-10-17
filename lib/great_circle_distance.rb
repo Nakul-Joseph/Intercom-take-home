144b6f94fd6aeefc1e1bde0b4b16d0f08ba0e733
nakul <nakuljsph@gmail.com>
2020-10-17 16:24:30 +0100

    refactor distance calc with havesine

--- lib/great_circle_distance.rb
+++ lib/great_circle_distance.rb
@@ -3,32 +3,34 @@ require 'byebug'
 class GreatCircleDistance
   EARTH_RADIUS = 6378.137
 
-  attr_reader :from, :to
+  attr_reader :lat1, :lon1, :lat2, :lon2
 
   def initialize(from:, to:)
-    @from = from
-    @to = to
+    @lat1, @lon1 = extract_lat_lon(from)
+    @lat2, @lon2 = extract_lat_lon(to)
   end
 
   def calculate
-    lat1, lon1, lat2, lon2 = [ from[:latitude].to_f, from[:longitude].to_f,
-                               to[:latitude].to_f, to[:longitude].to_f ].map{ |degrees| to_radians(degrees) }
-    central_angle = central_angle(lat1,lon1,lat2,lon2)
     (central_angle * EARTH_RADIUS).round(3)
   end
 
   private
 
+  def extract_lat_lon(resource)
+    [ resource[:latitude].to_f, 
+      resource[:longitude].to_f ].map { |deg| to_radians(deg) }
+  end
+
   def to_radians(degrees)
     degrees * (Math::PI/180)
   end
 
-  def central_angle(lat1,lon1,lat2,lon2)
-    # 2*arcsin(sqrt( Sin2(deltalat/2) + Coslat1 * Coslat2 * Sin2(deltalon/2))
+  # implemented haversine formula under 1.1 from https://en.wikipedia.org/wiki/Great-circle_distance
+  def central_angle
     delta_lon = (lon2 - lon1).abs
-    delte_lat = (lat2 - lat1).abs
-    #Math.acos((Math.sin(lat1) * Math.sin(lat2)) + (Math.cos(lat1) * Math.cos(lat2) * Math.cos(delta_lon)))
-    eqn = (Math.sin(delte_lat/2)**2) + (Math.cos(lat1) * Math.cos(lat2) * (Math.sin(delta_lon/2)**2))
+    delta_lat = (lat2 - lat1).abs
+
+    eqn = (Math.sin(delta_lat/2)**2) + (Math.cos(lat1) * Math.cos(lat2) * (Math.sin(delta_lon/2)**2))
     2 * Math.asin(Math.sqrt(eqn))
-  end
+   end
 end
